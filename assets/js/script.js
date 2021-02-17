8de2f9b13bef012841ecfff7af182f6380947729
Maria M <prov21_1@hotmail.com>
2021-02-17 10:24:05 -0700

    event object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 // select the ul and give it a var
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // create a function for the event listener
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    // keep page from refreshing and deleting the task
+    event.preventDefault();
     // create a new task item
     var listItemEl = document.createElement("li");
     // give the new li a class
@@ -15,7 +17,7 @@ var createTaskHandler = function() {
 };
 
 // run the button function and pass through the create a list item function above
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
 
